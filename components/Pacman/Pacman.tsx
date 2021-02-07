c2be833a5bb2bbd5db98d0e83228f8d7216753fa
delrotf <delrotf02@gmail.com>
2021-02-07 09:38:08 -0500

    1 file modified

--- components/Pacman/Pacman.tsx
+++ components/Pacman/Pacman.tsx
@@ -11,7 +11,11 @@ const StyledDiv = styled.div`
 `;
 
 const Pacman = props => {
-  const speed = 10;
+  const speed = 1;
+  const xMins = [0];
+  const xMaxes = [500];
+  const yMins = [0];
+  const yMaxes = [500];
   const rightDowned = useRef(false);
   const leftDowned = useRef(false);
   const upDowned = useRef(false);
@@ -24,6 +28,10 @@ const Pacman = props => {
 
   const keyDownHandler = ({ key }) => {
     console.log("downed key", key);
+    rightDowned.current = false;
+    leftDowned.current = false;
+    upDowned.current = false;
+    downDowned.current = false;
 
     switch (key) {
       case "ArrowRight":
@@ -37,28 +45,6 @@ const Pacman = props => {
         break;
       case "ArrowDown":
         downDowned.current = true;
-        break;
-      default:
-        console.log("Do nothing");
-        return;
-    }
-  };
-
-  const keyUpHandler = ({ key }) => {
-    console.log("uped key", key);
-
-    switch (key) {
-      case "ArrowRight":
-        rightDowned.current = false;
-        break;
-      case "ArrowLeft":
-        leftDowned.current = false;
-        break;
-      case "ArrowUp":
-        upDowned.current = false;
-        break;
-      case "ArrowDown":
-        downDowned.current = false;
         break;
       default:
         console.log("Do nothing");
@@ -81,28 +67,51 @@ const Pacman = props => {
     const interval = setInterval(() => {
       if (rightDowned.current) {
         console.log("while right", rightDowned.current);
-        setXPosition(prev => (prev += speed));
+        setXPosition(prev => {
+          if (!xMaxes.includes(prev)) {
+            return (prev += speed);
+          } else {
+            return prev;
+          }
+        });
       }
       if (leftDowned.current) {
-        setXPosition(prev => (prev -= speed));
+        setXPosition(prev => {
+          if (!xMins.includes(prev)) {
+            return (prev -= speed);
+          } else {
+            return prev;
+          }
+        });
       }
       if (upDowned.current) {
-        setYPosition(prev => (prev -= speed));
+        setYPosition(prev => {
+          if (!yMins.includes(prev)) {
+            return (prev -= speed);
+          } else {
+            return prev;
+          }
+        });
       }
       if (downDowned.current) {
-        setYPosition(prev => (prev += speed));
+        setYPosition(prev => {
+          if (!yMaxes.includes(prev)) {
+            return (prev += speed);
+          } else {
+            return prev;
+          }
+        });
       }
 
       return () => {
         clearInterval(interval);
       };
-    }, 500);
+    }, 1500);
   }, []);
 
   loopPosition();
 
   useEventListener("keydown", keyDownHandler);
-  useEventListener("keyup", keyUpHandler);
 
   return (
     <div className="Pacman">
